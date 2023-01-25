6971c6c0c4e252a406a623c024fe97413f5a8bc2
Rafael Carmona <dev.rccastruita@gmail.com>
2023-01-25 05:45:15 +0000

    useRepositories with graphql

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,25 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useQuery } from '@apollo/client';
+
+import { GET_REPOSITORIES } from '../graphql/queries';
 
 const useRepositories = () => {
-  const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  //const [repositories, setRepositories] = useState();
+  const { data, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: 'cache-and-network',
+  });
 
-  const fetchRepositories = async () => {
-    setLoading(true);
+  const repositories = loading ? null : data.repositories;
 
-    const address = '192.168.1.74';
-    const response = await fetch(`http://${address}:5000/api/repositories`);
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
-  };
-
-  useEffect(() => {
-    fetchRepositories();
-  }, []);
-
-  return { repositories, loading, refetch: fetchRepositories };
+  return { repositories, loading, refetch: refetch };
 };
 
 export default useRepositories;
