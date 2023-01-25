60de51ac24122b7494db89ee2822c810e5f940a5
Rafael Carmona <dev.rccastruita@gmail.com>
2023-01-25 05:20:13 +0000

    useRepositories hook with REST fetch

---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from 'react';
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+  const [loading, setLoading] = useState(false);
+
+  const fetchRepositories = async () => {
+    setLoading(true);
+
+    const address = '192.168.1.74';
+    const response = await fetch(`http://${address}:5000/api/repositories`);
+    const json = await response.json();
+
+    setLoading(false);
+    setRepositories(json);
+  };
+
+  useEffect(() => {
+    fetchRepositories();
+  }, []);
+
+  return { repositories, loading, refetch: fetchRepositories };
+};
+
+export default useRepositories;
